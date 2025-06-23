2a684d3978dc898365359c5bcd54ebe17547df83
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2025-06-23 20:04:45 +0100

    chore: fix len

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,8 @@
-export function stringToInts(str: string, size = 128): number[] {
+export function stringToInts(str: string, size = 127): number[] {
   let utf8Encode = new TextEncoder();
   // Pad this out to have a length of 128 and error if it's too long
   let ints = Array.from(utf8Encode.encode(str));
-  if (ints.length >= size) {
+  if (ints.length > size) {
     throw new Error("Term is too long");
   }
   return ints.concat(Array(size - ints.length).fill(0));
